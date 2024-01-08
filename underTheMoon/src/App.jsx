de4304fe2f4de7ed597fac7ec0b3fffa5149df8f
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2024-01-08 11:19:49 +0100

    gallery created #7

--- underTheMoon/src/App.jsx
+++ underTheMoon/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.scss'
 import { Header } from './components/header/Header'
+import { Main } from './components/Main/Main'
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <Main></Main>
 
     </>
   )
