4362d5fe63f53a95f92f3684abc9f34da194da42
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2024-01-08 13:11:08 +0100

    return på gallery

--- underTheMoon/src/components/gallery/Gallery.jsx
+++ underTheMoon/src/components/gallery/Gallery.jsx
@@ -3,15 +3,19 @@ import style from './Gallery.module.scss';
 export const Gallery = props => {
   return (
        <div className={style.galleri}>
-      {props.pictures && props.pictures.map((picture, index) => (
-        <img 
-            key={index} 
-            src={picture} 
-            alt={`Billede ${picture}`} 
-            title={`${picture.substr(picture.lastIndexOf('/')+1).replace('.jpg',"")}`}
-            />
+      {props.pictures && props.pictures.map((picture, index) => {
+        return (
+          <img 
+              key={index} 
+              src={picture} 
+              alt={`Billede ${picture}`} 
+              title={`${picture.substr(picture.lastIndexOf('/')+1).replace('.jpg',"")}`}
+              />
+
+        )
+      }
             
-      ))}
+      )}
     </div>
   );
 };
